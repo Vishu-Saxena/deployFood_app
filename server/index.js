6eab17b79736e118df0693162ce98ee2bcd38c6f
Vishu_Saxena <vishusaxena3110@gmail.com>
2025-06-03 11:56:54 +0530

    created vercel.json

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const userRoute = require("./router/routeuser");
 const foodRoute = require("./router/foodroute");
-const orderHistory = require('./router/orderRoute')
-const cors = require('cors')
+const orderHistory = require('./router/orderRoute');
+const cors = require('cors');
 // importing connection function
 const DB_connection = require("./dbconn");
 const dotenv = require("dotenv");
@@ -10,7 +10,13 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {
+        origin: [""],
+        methods: ["POST", "GET" , "PUT" , "DELETE"],
+        credentials: true
+    }
+));
 
 DB_connection(); // invoked databse connection
 app.get('/' , (req ,res)=>{
